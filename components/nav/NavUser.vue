8ac1d08fa2f8237d5a62846eb51b77acc95a8eba
Joseph Perez <joperez@dons.usfca.edu>
2023-09-08 10:34:37 -0700

    fix(header): add mobile create account button

--- components/nav/NavUser.vue
+++ components/nav/NavUser.vue
@@ -19,16 +19,34 @@ const { busy, oauth, singleInstanceServer } = useSignIn()
     </template>
   </VDropdown>
   <template v-else>
-    <button
+    <div
       v-if="singleInstanceServer"
       flex="~ row"
-      gap-x-1 items-center justify-center btn-solid text-sm px-2 py-1 xl:hidden
-      :disabled="busy"
-      @click="oauth()"
+      gap-x-1 items-center justify-center pl-28px pr-2 xl:hidden
     >
-      {{ $t('action.sign_in') }}
-    </button>
-    <button v-else btn-solid text-sm px-2 py-1 text-center xl:hidden @click="openSigninDialog()">
+      <button
+        flex="~ row"
+        mr-8px
+        gap-x-1 items-center justify-center btn-solid text-sm font-600 p-x-11px p-y-11px xl:hidden
+        b-rd-8px
+        :disabled="busy"
+        @click="oauth('signup')"
+      >
+        {{ $t('action.create_account') }}
+      </button>
+      <button
+        flex="~ row"
+        gap-x-1 items-center justify-center text-sm p-x-10px p-y-10px xl:hidden
+        border-1 border-primary btn-outline
+        text-primary text-center font-600
+        b-rd-8px
+        :disabled="busy"
+        @click="oauth()"
+      >
+        {{ $t('action.sign_in') }}
+      </button>
+    </div>
+    <button v-else btn-solid text-sm font-600 p-x-10px p-y-10px text-center xl:hidden @click="openSigninDialog()">
       {{ $t('action.sign_in') }}
     </button>
   </template>
